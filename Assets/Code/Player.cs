cf0c4223588e46a611a7d5e8b3b8d3888e46d90d
jsoranno15 <jsoranno15@gmail.com>
2023-04-13 14:06:34 -0400

    title screen, player movement and animations, ambient sounds, tile map with collisions, main level setup

---
+++ Assets/Code/Player.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float moveSpeed = 5f;
+    private Animator anim;
+    private Rigidbody2D rb;
+    Vector2 movement;
+    
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+        anim.SetFloat("Horizontal" , movement.x);
+        anim.SetFloat("Vertical" , movement.y);
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+}
