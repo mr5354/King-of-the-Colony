9544480e60192c151c8f00664b12b74011368d78
Sophie <lucyxinyuzhang@gmail.com>
2023-04-15 14:52:30 -0400

    added timer for minigame

--- Assets/Code/GameManager.cs
+++ Assets/Code/GameManager.cs
@@ -4,16 +4,28 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.SceneManagement;
+// using Random;
 
 public class GameManager : MonoBehaviour
 {
     private int happiness = 100;
     private HappinessBar bar;
-    private float countdownTime = 5f * 60f; // 5 minutes in seconds
+    private float countdownTime = 5f * 60f + 1f; // 5 minutes in seconds
     private bool TimerOn = false;
     public TextMeshProUGUI timerText;
     public string sceneToLoad;
+
+    // minigames
+    public static GameObject chewWireGame;
+    public static GameObject garbageCollectionGame;
+    public static GameObject fakeInfoGame;
+    public static GameObject minigame4;
+    public static GameObject minigame5;
     
+    private static GameObject[] minigames = {chewWireGame, garbageCollectionGame, fakeInfoGame, minigame4, minigame5};
+
+    private static bool activated = false;
+
     void Start()
     {
         // Find the HappinessBar object in the scene and get a reference to it
@@ -23,6 +35,7 @@ public class GameManager : MonoBehaviour
         bar.SetMaxHappiness(happiness);
 
         TimerOn = true;
+
     }
 
     void Update()
@@ -36,7 +49,38 @@ public class GameManager : MonoBehaviour
                 TimerOn = false;
                 SceneManager.LoadScene(sceneToLoad);
             }
+
+            // Debug.Log("can i activate: " + !activated);
+
+            if ((int)countdownTime % 10 == 0 && countdownTime > 0 && !activated) {
+                MinigameActivator();
+                activated = true;
+            } else if ((int)countdownTime % 10 != 0) {
+                activated = false;
+            }
         }
+    }
+
+    // every minute, a random number of minigames will be activated. if none are activated, at least one will be
+    void MinigameActivator() {
+        // create an array of size 5
+        bool[] minigameArray = new bool[5];
+        int count = 0;
+        for (int i = 0; i < minigameArray.Length; i++) {
+            // geenrate random true or false value
+            minigameArray[i] = Random.value < 0.5f;
+            if (minigameArray[i]) {
+                // activate minigame
+                count++;
+            }
+        }
+        Debug.Log("# of minigame activated: " + count);
+        // if no minigames are activated, activate at least one
+        if (count == 0) {
+            minigameArray[Random.Range(0, 5)] = true;
+            // activate this minigame
+        }
+
     }
     private string FormatTime(float time)
     {
