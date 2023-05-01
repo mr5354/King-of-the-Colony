523cac0ee63cf41915b2310400001d53fd3cecba
jsoranno15 <jsoranno15@gmail.com>
2023-05-01 16:13:46 -0400

    mini game template on scene 4

---
+++ Assets/Code/CloseOverlay.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CloseOverlay : MonoBehaviour
+{
+    public void CloseScene(){
+        Destroy(gameObject);
+    }
+}
